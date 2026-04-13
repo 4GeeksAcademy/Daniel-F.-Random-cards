edb93118813a883cbe7a0099ee9324bf8b9a42b2
Daniel Fabelo <daniel-fabelo-santana@outlook.es>
2026-04-13 19:16:55 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -9,3 +9,16 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let palos = ['♠','♥','♦','♣'];
+let valores = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
+
+function sacar() {
+  let palo = palos[Math.floor(Math.random()*4)];
+  let valor = valores[Math.floor(Math.random()*13)];
+
+  let carta = document.getElementById('carta');
+  carta.textContent = valor + ' ' + palo;
+
+  carta.style.color = (palo === '♥' || palo === '♦') ? 'red' : 'black';
+}
