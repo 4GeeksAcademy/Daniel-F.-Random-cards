06a80cbccfbec48da57f1b9e5d81f055637b8abd
Daniel Fabelo <daniel-fabelo-santana@outlook.es>
2026-04-15 18:37:06 +0000

    Arreglado!

--- src/app.js
+++ src/app.js
@@ -5,20 +5,32 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
-
 let palos = ['♠','♥','♦','♣'];
 let valores = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
 
-function sacar() {
-  let palo = palos[Math.floor(Math.random()*4)];
-  let valor = valores[Math.floor(Math.random()*13)];
+function generarCarta() {
+  let palo = palos[Math.floor(Math.random() * palos.length)];
+  let valor = valores[Math.floor(Math.random() * valores.length)];
 
-  let carta = document.getElementById('carta');
-  carta.textContent = valor + ' ' + palo;
+  let carta = document.getElementById("carta");
+  let paloArriba = document.getElementById("palo-arriba");
+  let paloAbajo = document.getElementById("palo-abajo");
+  let valorCarta = document.getElementById("valor");
 
-  carta.style.color = (palo === '♥' || palo === '♦') ? 'red' : 'black';
+  paloArriba.innerHTML = palo;
+  paloAbajo.innerHTML = palo;
+  valorCarta.innerHTML = valor;
+
+  // color según palo -
+  if (palo === '♥' || palo === '♦') {
+    carta.classList.add("rojo");
+    carta.classList.remove("negro");
+  } else {
+    carta.classList.add("negro");
+    carta.classList.remove("rojo");
+  }
 }
+
+// Generar una carta al cargar
+onclick = generarCarta;
+window.onload = generarCarta;
